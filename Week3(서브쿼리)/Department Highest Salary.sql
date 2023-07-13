ed19db882d3f52ae8290f736d2c5fad5360daa30
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-07-13 18:35:54 +0900

    Update Department Highest Salary.sql

--- Week3(서브쿼리)/Department Highest Salary.sql
+++ Week3(서브쿼리)/Department Highest Salary.sql
@@ -1,1 +1,20 @@
+# sol_1
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+     INNER JOIN Department d ON e.departmentId = d.id
+WHERE (salary, departmentId) IN(
+     SELECT MAX(salary), departmentId
+     FROM Employee
+     GROUP BY departmentId)
 
+# sol_2
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+     INNER JOIN(SELECT departmentId, Max(salary) as max_salary
+                FROM Employee
+                GROUP BY departmentId) AS sub ON e.departmentId = sub.departmentId AND e.salary = sub.max_salary
+     INNER JOIN Department d ON e.departmentid = d.id
