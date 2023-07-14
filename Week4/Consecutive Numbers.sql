5e6c37024f6a93d7c33fb3f1ac66137dd718dc38
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-07-14 18:11:16 +0900

    Update Consecutive Numbers.sql

--- Week4/Consecutive Numbers.sql
+++ Week4/Consecutive Numbers.sql
@@ -1,5 +1,5 @@
-[문제링크](https://leetcode.com/problems/consecutive-numbers/)
-  
+문제링크: https://leetcode.com/problems/consecutive-numbers/
+# MS SQL Server
 WITH sub AS(SELECT num,
             LEAD(num,1) OVER (ORDER BY id) AS next,
             LEAD(num,2) OVER (ORDER BY id) AS afternext
