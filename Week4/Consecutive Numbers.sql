2798438f9a8ad28230405b50155a71d5fbf4f8e7
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-07-14 18:10:39 +0900

    Create Consecutive Numbers.sql

---
+++ Week4/Consecutive Numbers.sql
@@ -0,0 +1,10 @@
+[문제링크](https://leetcode.com/problems/consecutive-numbers/)
+  
+WITH sub AS(SELECT num,
+            LEAD(num,1) OVER (ORDER BY id) AS next,
+            LEAD(num,2) OVER (ORDER BY id) AS afternext
+FROM Logs)
+
+SELECT num AS ConsecutiveNums
+FROM sub
+WHERE num = next AND next = afternext
