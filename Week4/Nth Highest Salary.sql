142e4585004b0ca44498fdefc21207cfba2f346d
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-07-14 21:55:40 +0900

    Update Nth Highest Salary.sql

--- Week4/Nth Highest Salary.sql
+++ Week4/Nth Highest Salary.sql
@@ -12,3 +12,20 @@ BEGIN
                  LIMIT N) AS sub);
 
 SELECT getNthHighestSalary(3)
+
+# LIMIT 심화 사용
+# 파라미터를 LIMIT 안에서 N-1로 직접적인 조작은 불가
+# 따라서 DECLARE,SET을 이용해야 함
+CREATE FUNCTION getNthHighestSalary(N INT)
+RETURNS INT
+BEGIN 
+   DECLARE A INT; # 직접적인 파라미터 조작 불
+   SET N = N-1;
+   RETURN(SELECT DISTINCT salary
+          FROM Employee
+          ORDER BY salary DESC
+          LIMIT N, 1
+          # LIMIT 1 OFFESET N);
+END
+
+SELECT getNthHighestSalary(3)
